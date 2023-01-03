aadf6cb782ff6026c6ed77bc7bfe6acaa847e441
Angelo Chaknis <angelopalmtree@gmail.com>
2023-01-02 21:24:37 -0700

    feat: add GameController Class

---
+++ app.js
@@ -0,0 +1,18 @@
+class GameController {
+  constructor(word, life) {
+    this.word = word;
+    this.life = life;
+  }
+
+  setUi() {
+    // Update screen to show letter boxes
+  }
+
+  currentLife() {
+    // show how many lives you currently have
+  }
+
+  decreaseLife() {
+    // subtract lives
+  }
+}
